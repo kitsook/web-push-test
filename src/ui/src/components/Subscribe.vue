54d2a7d33520d04eb4b8eda92ac61d526cd5518a
Clarence Ho <clarenceho@gmail.com>
2024-01-28 12:32:22 -0800

    Add details on permission

--- src/ui/src/components/Subscribe.vue
+++ src/ui/src/components/Subscribe.vue
@@ -12,9 +12,21 @@
           Current permission: <span class="font-weight-bold"> {{ notificationPermission }} </span>
           </p>
 
-          <p class="font-italic">
-          Note that if permission is "denied", browser will not prompt for subscription. Please change it on site information.
-          </p>
+          <div v-show="notificationPermission == 'denied'">
+              <p class="font-italic mb-2">
+              Note that if permission is "denied", browser will not prompt for subscription. Please enable it in Settings and reload the page.
+              </p>
+
+              <v-btn class="ma-2" href="https://support.microsoft.com/en-us/microsoft-edge/manage-website-notifications-in-microsoft-edge-0c555609-5bf2-479d-a59d-fb30a0b80b2b" target="_blank">
+                Edge
+              </v-btn>
+              <v-btn class="ma-2" href="https://support.google.com/chrome/answer/3220216?hl=en&co=GENIE.Platform%3DDesktop#zippy=%2Cfix-issues-with-notifications" target="_blank">
+                Chrome
+              </v-btn>
+              <v-btn class="ma-2" href="https://support.mozilla.org/en-US/kb/push-notifications-firefox" target="_blank">
+                Firefox
+              </v-btn>
+          </div>
         </v-card>
       </v-col>
 
